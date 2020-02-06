4d55c2c917a8fd0c9ed8e1da792b1ba4c8abbaec
Mark Stoke <mark.a.stoke@gmail.com>
2020-02-06 03:46:05 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  square = Array.new
+  array.each { |ele| square << ele**2 }
+  square
+end
+
+def square_array_map(array)
+  square = Array.new
+  array.map { |ele| square << ele**2 if ele % 2 == 1}
+  square
 end
